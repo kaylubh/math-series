b956e82cb3e7e7eac79ece9955bbaf509c3034b9
Caleb Hemphill <calebhemp@gmail.com>
2024-01-10 22:06:38 -0500

    Add docstrings to series functions

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,13 @@
 def fibonacci(n):
+  """
+  Generates a number at a requested index(`n`) from the Fibonacci sequence. `n` begins at the 0 index.
+
+  Parameters:
+  `n` (integer): The index of the Fibonacci sequence numbers to return
+
+  Returns: 
+  integer: The number from the Fibonacci sequence at the requested index
+  """
 
   if type(n) != int:
     return None
@@ -16,6 +25,15 @@ def fibonacci(n):
 
 
 def lucas(n):
+  """
+  Generates a number at a requested index(`n`) from the Lucas sequence. `n` begins at the 0 index.
+
+  Parameters:
+  `n` (integer): The index of the Lucas sequence numbers to return
+
+  Returns: 
+  integer: The number from the Lucas sequence at the requested index
+  """
 
   if type(n) != int:
     return None
@@ -33,6 +51,18 @@ def lucas(n):
 
 
 def sum_series(n, n_0 = 0, n_1 = 1):
+  """
+  Generates a number at a requested index(`n`) from a sequence which sums the preceding two numbers in the series. Defaults to the Fibonacci sequence if the two starting numbers of the sequence are not provided. `n` begins at the 0 index.
+  `n` = (`n` - 1) + (`n` - 2)
+
+  Parameters:
+  `n` (integer): The index of the sequence numbers to return
+  `n_0` (integer)(default = 0): The first number in the sequence
+  `n_1` (integer)(default = 1): The second number in the sequence
+
+  Returns: 
+  integer: The number from the sequence at the requested index
+  """
 
   if type(n) != int:
     return None
